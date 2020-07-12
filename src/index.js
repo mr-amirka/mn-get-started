ae6beb1aadba36a67a5ae66f52ac860102fd85bb
Amir Absolutely <mr.amirka@ya.ru>
2020-07-12 19:31:20 +0300

    v0.0.12

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 require('./style.mn.js');
 const React = require('react');
 const {render} = require('react-dom');
+const get = require('mn-utils/get');
+const detection = require('mn-utils/browser/detection');
 
+document.body.className = detection(get(window, 'navigator.userAgent'));
 render(<App/>, document.getElementById('app'));
+
 
 function App() {
   return (
@@ -19,7 +23,7 @@ function App() {
           <div className="p15 pb65 rlv tc">
             <div className="f30">Minimalist Notation</div>
             <div className="fCustom fCustomBig*2">Example</div>
-            <div className="sq40 rlv bg0 spnr3000"/>
+            <div className="sq40 rlv bg0 spnr3000 mhA mt50"/>
             <div className="abs shb bs b0 bt1 bcC bgF">
               <div className="tbl w h50">
                 <div className="ph15">
--- src/style.mn.js
+++ src/style.mn.js
@@ -1,18 +1,6 @@
 module.exports = (mn) => {
-  /*
-  mn({
-    ffTheme: {
-      style: {
-        fontFamily: "'Exo Two', 'Open Sans', 'Roboto', Arial, sans-serif"
-      }
-    }
-  });
-  mn.assign({
-    img: 'wmax'
-  });
   mn({
     fCustom: 'f50',
-    fCustomBig: 'f100'
+    fCustomBig: 'f100',
   });
-  */
 };
